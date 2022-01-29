80d5bc025ac8299bcfb7c842a3aa7a74ec060109
Deezuleets <daniel.liivak@gmail.com>
2022-01-29 13:37:04 +0200

    Fixed print formatting

--- main.lua
+++ main.lua
@@ -8,7 +8,7 @@ function onPlayerAuth(name)
 	local f = assert(io.open("./Resources/Server/banman-lite-whitelist/whitelist", "r"))
 	local t = f:read ("*all")
 	
-	print("Checking whitelist for", name)
+	print("Checking whitelist for " .. name)
 	
 	if not string.match(t, name) then
 		return "You have not been whitelisted on this server."
